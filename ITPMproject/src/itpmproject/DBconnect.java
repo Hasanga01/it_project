96bf11d4f349e10ad095cd04a7a6210555ef5510
gayathriimesha1998 <68746283+gayathriimesha1998@users.noreply.github.com>
2021-05-21 11:44:22 +0530

    Add files via upload

---
+++ ITPMproject/src/itpmproject/DBconnect.java
@@ -0,0 +1,34 @@
+package itpmproject;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author priya
+ */
+public class DBconnect {
+
+    public static Connection connect()
+     {
+
+	Connection conn = null;
+
+	try
+	{
+	  Class.forName("com.mysql.jdbc.Driver");
+	  conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/details" ,"root","");
+
+	}
+
+	catch(Exception e)
+	{
+		System.out.println(e);
+
+	}
+
+
+	return conn;
+      }
+    
+}
